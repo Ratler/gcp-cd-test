7b321111b774050b8b240db5c1c8ad9a756db790
Stefan Wold <stefan.wold@yubico.com>
2018-04-12 13:44:35 +0200

    Added build step

--- run-tests.sh
+++ run-tests.sh
@@ -13,7 +13,22 @@ if [ "$1" = "test" ]; then
   cd src/github.com/Ratler/gcp-cd-test
   ls -l
   go test -cover ./... | tee test_coverage.txt
-  mv test_coverage.txt $GOPATH/
+  mv test_coverage.txt $GOPATH/gcp-cd-test
+elif [ "$1" = "buildgo" ]; then
+  export GOPATH=$PWD
+  mkdir -p src/github.com/Ratler $PWD/dist
+  ln -sf $PWD/gcp-cd-test src/github.com/Ratler/gcp-cd-test
+  echo "Gopath is: " $GOPATH
+  echo "pwd is: " $PWD
+  cd src/github.com/Ratler/gcp-cd-test
+  go build -o $GOPATH/dist/gcp-cd-test ./main.go
+  cp Dockerfile $GOPATH/dist/Dockerfile
+  cd $GOPATH
+  ls -l dist/
+elif [ "$1" = "build" ]; then
+  docker run --rm -v $PWD:/go/gcp-cd-tes golang:1.7.1 bash /go/gcp-cd-test/run.sh buildgo
+  cd dist
+  docker build . -t gcp-cd-test:latest
 else
-  docker run --rm -t -v $PWD:/go/gcp-cd-test golang:1.7.1 bash /go/gcp-cd-test/run-tests.sh test
+  docker run --rm -t -v $PWD:/go/gcp-cd-test golang:1.7.1 bash /go/gcp-cd-test/run.sh test
 fi
