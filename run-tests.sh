9c93ba5cfd3b5778226ae4b1ef27107a5a0a9624
Stefan Wold <stefan.wold@yubico.com>
2018-04-12 12:20:41 +0200

    Quick hack to work with GOCD

--- run-tests.sh
+++ run-tests.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 set -e -x -o pipefail
-echo "pwd is: $PWD"
-export GOPATH=$PWD
-ls -l
-mkdir -p src/github.com/Ratler
-ln -sf $PWD/gcp-cd-test src/github.com/Ratler/gcp-cd-test
-echo "Gopath is: " $GOPATH
-echo "pwd is: " $PWD
-cd src/github.com/Ratler/gcp-cd-test
-ls -l
-go test -cover ./... | tee test_coverage.txt
-mv test_coverage.txt $GOPATH/coverage-results/
+
+if [ "$1" = "test" ]; then
+  echo "pwd is: $PWD"
+  export GOPATH=$PWD
+  ls -l
+  mkdir -p src/github.com/Ratler
+  ln -sf $PWD/gcp-cd-test src/github.com/Ratler/gcp-cd-test
+  echo "Gopath is: " $GOPATH
+  echo "pwd is: " $PWD
+  cd src/github.com/Ratler/gcp-cd-test
+  ls -l
+  go test -cover ./... | tee test_coverage.txt
+  mv test_coverage.txt $GOPATH/coverage-results/
+else
+  docker run --rm -t -v $PWD:/data golang:1.7.1 bash run-tests.sh test
+fi
