767c5aadf34cb3a482522e8002c544eaa17328e5
Stefan Wold <ratler@stderr.eu>
2018-04-12 13:48:58 +0200

    copy coverage

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ if [ "$1" = "test" ]; then
   cd src/github.com/Ratler/gcp-cd-test
   ls -l
   go test -cover ./... | tee test_coverage.txt
-  mv test_coverage.txt $GOPATH/gcp-cd-test
+  cp test_coverage.txt $GOPATH/gcp-cd-test
 elif [ "$1" = "buildgo" ]; then
   export GOPATH=$PWD
   mkdir -p src/github.com/Ratler $PWD/dist
